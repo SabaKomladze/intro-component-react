bf17fa85cf22facc43c4bf71782b4ae939b9b80b
Saba Komladze <sabakomladze2@gmail.com>
2023-05-24 15:19:27 +0400

    mobile version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <section className="leftSide">
+        <h1 className="learnToCode">Learn to code by watching others</h1>
+        <p className="seeHow">
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+
+      <section className="form">
+        <div className="purple">
+          <span className="days">Try it free 7 days</span> then $20/mo.
+          thereafter
+        </div>
+        <form>
+          <input placeholder="First Name" />
+          <input placeholder="Last Name" />
+          <input placeholder="Email Adress" />
+          <input placeholder="Password" />
+          <button type="submit">CLAIM YOUR FREE TRIAL</button>
+          <p className="terms">
+            By clicking the button, you are agreeing to our
+            <span className="service"> Terms and Services</span>
+          </p>
+        </form>
+      </section>
     </div>
   );
 }
